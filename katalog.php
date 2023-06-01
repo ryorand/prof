cb4afde7ac973bf9fada75814e15358e5587d20a
ryorand <135257455+ryorand@users.noreply.github.com>
2023-06-01 19:45:40 +0300

    Add files via upload

---
+++ katalog.php
@@ -0,0 +1,112 @@
+!<!DOCTYPE html>
+<html lang ="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-widht, initial-scale = 1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+	<title>Scissors and thread</title>
+</head>
+<body>
+	<?php require "blocks/header.php" ?>
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div class="col">
+            <div class="card shadow-sm">
+              <img src="img/5.jpg" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Пальто</svg>
+              <div class="card-body">
+                <p class="card-text">Зимнее женское пальто из лучших тканей.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <a class="btn btn-outline-secondary" type="button"  href="/palt.php">Подробнее</a>
+
+                  </div>
+                  <small class="text-body-secondary">700$</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card shadow-sm">
+              <img src="img/6.jpg" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Рубашка</text></svg>
+              <div class="card-body">
+                <p class="card-text">Синяя мужская рубашка в клетку.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                          <a class="btn btn-outline-secondary" type="button"  href="/tish.php">Подробнее</a>
+
+                  </div>
+                  <small class="text-body-secondary">120$</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card shadow-sm">
+              <img src="img/7.png" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Мужской набор на лето</text></svg>
+              <div class="card-body">
+                <p class="card-text">Шорты,футболка,обувь.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <a class="btn btn-outline-secondary" type="button"  href="/nab.php">Подробнее</a>
+
+                  </div>
+                  <small class="text-body-secondary">300$</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card shadow-sm">
+              <img src="img/5.jpg" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Пальто</text></svg>
+              <div class="card-body">
+                <p class="card-text">Зимнее женское пальто из лучших тканей.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+          <a class="btn btn-outline-secondary" type="button"  href="/palt.php">Подробнее</a>
+
+                  </div>
+                  <small class="text-body-secondary">700$</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card shadow-sm">
+              <img src="img/6.jpg" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Рубашка</text></svg>
+              <div class="card-body">
+                <p class="card-text">Синяя мужская рубашка в клетку.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+        <a class="btn btn-outline-secondary" type="button"  href="/tish.php">Подробнее</a>
+
+                  </div>
+                  <small class="text-body-secondary">120$</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card shadow-sm">
+              <img src="img/7.png"width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Мужской набор на лето</text></svg>
+              <div class="card-body">
+                <p class="card-text">Шорты,футболка,обувь.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+        <a class="btn btn-outline-secondary" type="button"  href="/nab.php">Подробнее</a>
+
+                  </div>
+                  <small class="text-body-secondary">300$</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+
+	<?php require "blocks/footer.php" ?>
+
+</body>
+</html>
